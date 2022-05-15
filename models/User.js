37e58c4d02d88dc38e853360299bb1e620bd3c34
HedgehogKU <ccc908925@gmail.com>
2022-05-15 10:44:39 +0800

    UPDATE: model User add trim, select, match

--- models/User.js
+++ models/User.js
@@ -5,31 +5,40 @@ const userSchema = new mongoose.Schema(
         name: {
             type: String,
             required: [true, '【暱稱】必填'],
+            trim: true,
         },
         sex: {
             type: String,
             enum: ['male', 'female'],
             required: [true, '【性別】必填'],
+            trim: true,
         },
         email: {
             type: String,
             required: [true, '【帳號】必填'],
+            trim: true,
         },
         password: {
             type: String,
             required: [true, '【密碼】必填'],
+            select: false,
+            trim: true,
         },
         avatar: {
             type: String,
-            default: 'https://i.imgur.com/m8khK3h.png'
+            default: 'https://i.imgur.com/m8khK3h.png',
+            match: /^https/g,
+            trim: true,
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            select: false,
         },
         updatedAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            select: false,
         }
     },
     {
